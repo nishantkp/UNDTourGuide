c9ca2fb076855932a9c0a7229a427e753524746d
Nishant Patel <nishantuwindsor@gmail.com>
2017-11-04 23:25:31 -0400

    Create a new activity for more detail about attraction

---
+++ app/src/main/java/com/example/android/undtourguide/AttractionDetail.java
@@ -0,0 +1,13 @@
+package com.example.android.undtourguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AttractionDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attraction_detail);
+    }
+}
