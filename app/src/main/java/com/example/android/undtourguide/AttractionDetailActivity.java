bcb923f1d53ef7277a1cc2b4e79520ae1b32bec2
Nishant Patel <nishantuwindsor@gmail.com>
2017-11-05 14:07:28 -0500

    Add button to show location on maps

--- app/src/main/java/com/example/android/undtourguide/AttractionDetailActivity.java
+++ app/src/main/java/com/example/android/undtourguide/AttractionDetailActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.undtourguide;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,7 +16,7 @@ public class AttractionDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_attraction_detail);
 
         // Get the intent
-        Bundle bundle = getIntent().getExtras();
+        final Bundle bundle = getIntent().getExtras();
 
         // Find the ImageView for the image of attraction by id of list_item_attraction_image
         ImageView attractionImage = findViewById(R.id.list_item_attraction_image);
@@ -34,5 +37,19 @@ public class AttractionDetailActivity extends AppCompatActivity {
         TextView attractionAddress = findViewById(R.id.list_item_attraction_address_to_google_maps);
         // Get the attraction address from Bundle and set the text
         attractionAddress.setText(bundle.getString(Keys.ATTRACTION_ADDRESS_KEY));
+
+        // Find the TextView with id of take_me_to_location and attach a listener to it
+        // So when user clicks on TextView, it opens up maps through Intent to show location
+        TextView openMaps = findViewById(R.id.take_me_to_location);
+        openMaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(bundle.getString(Keys.ATTRACTION_LOCATION_KEY)));
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
